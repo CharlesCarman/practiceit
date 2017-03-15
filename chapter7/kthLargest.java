8520e76782a05128bc9cafd155965302ed06c59f
CharlesCarman <chuck831@comcast.net>
2017-03-15 16:23:48 -0700

    You went way too far in to depth...
    All you have to do is sort it and find the kth from the end

--- chapter7/kthLargest.java
+++ chapter7/kthLargest.java
@@ -3,39 +3,7 @@
  * or equal value. If k = 0, return the largest element; if k = 1, return the 
  * second largest element, and so on. Assume that 0 <= k < a.length.
  */
-public int kthLargest(int k, int[] a) {
-    return select(a, 0, a.length - 1, a.length - k);
-}
-
-public int select(int[] a, int p, int r, int i) {
-    int q = partition(a, p, r);
-    int k = q - p + 1;
-    
-    if(k == i)
-        return a[q];
-        
-    if(i < k)
-        return select(a, p, q - 1, i);
-        
-    return select(a, q + 1, r, i - k);
-}
-
-public int partition(int[] a, int p, int r) {
-    int temp;
-    int x = a[r];
-    int i = p - 1;
-    
-    for(int j = p; j <= r - 1; j++) {
-        if(a[j] <= x) {
-            i++;
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-        }
-    }
-
-    temp = a[i + 1];
-    a[i + 1] = a[r];
-    a[r] = temp;
-    return i + 1;
+public static int kthLargest(int k, int[] a) {
+	Arrays.sort(a);
+    return a[(a.length - 1) - k];
 }
